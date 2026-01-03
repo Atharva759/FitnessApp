64256b7590166f5f4724d45eafcd390492f80d7e
Atharva <atharvakhadake759@gmail.com>
2026-01-03 10:34:48 +0530

    API Documentation

--- fitnessBackend/src/main/java/com/project/fitness/security/SecurityConfig.java
+++ fitnessBackend/src/main/java/com/project/fitness/security/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests.requestMatchers("/api/admin/**").hasRole("ADMIN")
                                 .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/swagger-ui/**","/swagger-ui.html","/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated());
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
